dcdd8ab2bbd13d08250512f21ecf30b72d8b0d68
Cristian Alejandro Cisneros Villegas <125778068+AlejandroCisnerosVillegas@users.noreply.github.com>
2024-06-11 13:56:58 -0600

    Add files via upload

--- agregar.php
+++ agregar.php
@@ -1,12 +1,7 @@
 <?php include_once("conexion.php"); 
-    
     $nombre 	= $_POST['txtnombre'];
     $correo 	= $_POST['txtcorreo'];
     $telefono 	= $_POST['txttelefono'];
-    
 	mysqli_query($conn, "INSERT INTO poject_17_usuarios(nom,correo,tel) VALUES('$nombre','$correo','$telefono')");
-    
-header("Location:index.php");
-	
-
+    header("Location:index.php");
 ?>
--- eliminar.php
+++ eliminar.php
@@ -1,10 +1,6 @@
 <?php
-include_once("conexion.php");
- 
-$cod = $_GET['cod'];
- 
-mysqli_query($conn, "DELETE FROM poject_17_usuarios WHERE cod=$cod");
- 
-header("Location:index.php");
-
+    include_once("conexion.php");
+    $cod = $_GET['cod'];
+    mysqli_query($conn, "DELETE FROM poject_17_usuarios WHERE cod=$cod");
+    header("Location:index.php");
 ?>
